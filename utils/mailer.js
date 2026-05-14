2b0bb5adeef3de4bf9712d87acd9911ce37690ee
harshit <munjalharshit0@gmail.com>
2026-05-14 12:01:33 +0530

    force IPv4 for nodemailer

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,10 +1,13 @@
 
 const nodemailer = require('nodemailer');
 
+
+
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 587,
     secure: false,
+    family: 4,  // force IPv4
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
